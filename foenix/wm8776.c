9802c9266df4b0c45662db06430158d059b59afb
Vincent Barrilliot <vincent.barrilliot@laposte.net>
2022-09-15 23:54:47 +0200

    Improve WM8776 code support

--- foenix/wm8776.c
+++ foenix/wm8776.c
@@ -1,14 +1,12 @@
 /*
- * WM8776 - Functions for the WM8776 Audio codec/mixer
- *
- * Copyright (C) 2013-2021 The EmuTOS development team
+ * WM8776 - Functions for the WM8776 Audio wm8776/mixer
  *
  * Authors:
  *  VB   Vincent Barrilliot
  *  PJW  Peter Weingartner
  *
  * This file is distributed under the GPL, version 2 or at your
- * option any later version.  See doc/license.txt for details.
+ * option any later version.
  */
 
 #include <stdint.h>
@@ -16,44 +14,73 @@
 #include "wm8776.h"
 
 
-/* We keep track of the last volume set */
-static uint8_t wm8776_volume;
+/* We can't read from the WM8776, so we shadow the registers. */
+static uint16_t wm8776_regs[23];
 
 /* Forward declarations */
 static void wm8776_wait(void);
 
 /* Convenience */
-#define DECLARE_codec volatile uint16_t *codec = WM8776_PORT
+static volatile uint16_t * const wm8776 = WM8776_PORT;
 
+/* Initialisation data for the WM8776 CODEC */
+static uint16_t wm8776_init_data[] = {
+    /* Master reset */
+    WM8776_R23_RESET,
+    /* Power options */
+    WM8776_R13_POWER_DOWN_CONTROL | PDWN_NORMAL | ADCPD_NORMAL | DACPD_NORMAL | HPPD_NORMAL | AINPD_NORMAL,
+    /* Input mixer, ADC mute */
+    WM8776_R21_ADC_INPUT_MIXER_AND_MUTE | AMX_AIN1 | AMX_AIN2 | AMX_AIN3 | AMX_AIN4 | AMX_AIN5 | MUTERA_NORMAL | MUTELA_NORMAL,
+    /* Auto-level control */
+    WM8776_R17_ADC_ANALOG_AUTO_LEVEL_CONTROL | LCEN | HLD_MASK & 1,
+    /* Output mixer */
+    WM8776_R22_OUTPUT_MIXER | MX_DAC | MX_AUX | MX_BYPASS,
+    /* DAC interface control */
+    WM8776_R10_DAC_INTERFACE_CONTROL | DACFMT_I2S,
+    /* ADC interface control */
+    WM8776_R11_ADC_INTERFACE_CONTROL | ADCFMT_I2S,
+    /* Master mode control */
+    WM8776_R12_MASTER_MODE_CONTROL | ADCRATE_768 | DACRATE_512,
+ };
 
-/* Initialisation data for the WM8776 CODEC
- * R13 - Turn On Headphones
- * R21 - Enable All the Analog In
- * R17, R22, R10, R11, R12 */
-static uint16_t wm8776_init_data[] = { 0x1A00,0x2A1F,0x2301,0x2C07,0x1402,0x1602,0x1845 };
 
 void wm8776_init(void)
-{        
+{
     int i;
+    uint16_t v;
     for (i = 0; i < sizeof(wm8776_init_data)/sizeof(uint16_t); i++)
+    {
+        v = wm8776_init_data[i];
         wm8776_send(wm8776_init_data[i]);
+    }
+}
+
+
+void wm8776_deinit(void)
+{
+    // TODO: fade out using DMUTE then power off, but it may require timer etc.
 }
 
 
 void wm8776_send(uint16_t data)
 {
-    DECLARE_codec;
+    wm8776_regs[WM8776_REGNR(data)] = WM8776_VALUE(data); 
     
-    wm8776_wait();
-    *codec = data;
+    /* Gavin/Beatrix take care of the communication with the WM8776 (how nice).
+     * We only need to write the 16 bits (14 of them are really relevant).
+     * MSB of the port tells us weither the line is busy, if so we have to wait.
+     *
+     * Possible enhancement: queue messages and use interrupts. */ 
+    while (*wm8776 & 0x8000)
+        ;
+    *wm8776 = data;
 }
 
 
 void wm8776_set_digital_volume(uint8_t volume)
-{    
-    wm8776_volume = volume;
-    wm8776_send(0x0A00 | (0xFF - (volume & 0xFF)));
-    wm8776_send(0x0400 | ((volume >> 1) & 0xff));
+{
+    wm8776_send(WM8776_R05_DAC_VOLUME_ALL | (0xFF - (volume & 0xFF)));
+    wm8776_send(WM8776_R02_HEADPHONE_ATTENUATION_ALL | ((volume >> 1) & 0xff));
 }
 
 
@@ -62,10 +89,3 @@ uint8_t wm8776_get_digital_volume(void)
     return wm8776_volume;
 }
 
-
-static void wm8776_wait(void)
-{
-    DECLARE_codec;
-    while (*codec & 0x8000)
-        ;
-}
