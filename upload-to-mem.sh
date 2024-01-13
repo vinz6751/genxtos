9fdc87316009b10974863ad84e05f472188f4422
vinz6751 <vincent.barrilliot@laposte.net>
2024-01-13 23:11:05 +0100

    Add script to upload EmuTOS to flash. fnxmgr should really be the invocation of the FoenixMgr python script

--- upload-to-mem.sh
+++ upload-to-mem.sh
@@ -1,3 +1,2 @@
-# Make sure the OS address is 0x100000 in emutos.ld
-# and poke 0x100000 to the 68k's reset vector at 0x4
-fnxmgr --binary emutos-a2560u.rom --address 0x100000
+#!/bin/sh
+fnxmgr --upload-srec ../EmuTOS-RAM-loader/ldemutos.s28 ; fnxmgr --binary emutos-a2560u.rom --address 0x100000
