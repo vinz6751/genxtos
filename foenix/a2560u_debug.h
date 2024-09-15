cf233d907b9bca7484e94cdf273353052f778caa
vinz6751 <vincent.barrilliot@laposte.net>
2024-09-15 14:16:39 +0200

    Include config.h so we can adapt the debugging to what's where.

--- foenix/a2560u_debug.h
+++ foenix/a2560u_debug.h
@@ -1,10 +1,10 @@
 #ifndef A2560U_DEBUG_H
 #define A2560U_DEBUG_H
 
+#include "config.h"
 #ifndef FOENIX_CHANNEL_A_DEBUG_PRINT
 #define FOENIX_CHANNEL_A_DEBUG_PRINT 1
 #endif
-#include "kprint.h"
 
 void a2560_debugnl(const char* __restrict__ s, ...);
 void a2560_debug(const char* __restrict__ s, ...);
