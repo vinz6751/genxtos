f19d6a98722ed96e92acc97828cc6aa7ee8c313a
Vincent B <vincent.barrilliot@laposte.net>
2024-12-01 10:51:45 +0100

    Minor whitespace and improvement.

--- bios/chardev.c
+++ bios/chardev.c
@@ -13,7 +13,6 @@
  */
 
 #include "emutos.h"
-#include "gemerror.h"
 #include "tosvars.h"
 #include "chardev.h"
 #include "conout.h"
@@ -52,6 +51,7 @@ LONG char_dummy(void)
     return 0L;
 }
 
+
 LONG charout_dummy(WORD dev,WORD b)
 {
     return 0L;
@@ -66,13 +66,12 @@ int i;
 
     /* initialise bios device vectors */
     for (i = 0; i < NUM_CHAR_VECS; i++)
+    {
         bconstat_vec[i] = bconstat_init[i];
-    for (i = 0; i < NUM_CHAR_VECS; i++)
         bconin_vec[i] = bconin_init[i];
-    for (i = 0; i < NUM_CHAR_VECS; i++)
         bcostat_vec[i] = bcostat_init[i];
-    for (i = 0; i < NUM_CHAR_VECS; i++)
         bconout_vec[i] = bconout_init[i];
+    }
 
     /* setup serial output functions */
     aux_stat = just_rts;
@@ -85,7 +84,6 @@ int i;
 }
 
 
-
 /* BIOS devices - bconout functions */
 
 LONG bconout2(WORD dev, WORD b)
@@ -93,6 +91,7 @@ LONG bconout2(WORD dev, WORD b)
     cputc(b);
     return 1L;
 }
+
 
 /* bconout5 - raw console output. */
 LONG bconout5(WORD dev, WORD ch)
@@ -104,7 +103,6 @@ LONG bconout5(WORD dev, WORD ch)
     conout_ascii_out(LOBYTE(ch));
     return 1L;
 }
-
 
 
 /* BIOS devices - bcostat functions */
