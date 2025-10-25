6ccba78e69d777e57fa829c3b8816e1f2f2ea287
vinz6751 <vincent.barrilliot@laposte.net>
2025-10-25 21:09:31 +0200

    Tidy up scripts to upload

--- upload-to-flash.sh
+++ upload-to-flash.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-FNXMGR="python3 $FOENIXMGR/FoenixMgr/fnxmgr.py --port /dev/ttyUSB1"
-fnxmgr --flash emutos-a2560m.rom --address 0x000000
+COM=/dev/ttyUSB1
+FNXMGR="python3 $FOENIXMGR/FoenixMgr/fnxmgr.py --port $COM"
+ROM=`ls emutos-a2560?.rom`
+echo "Uploading $ROM to flash through port $COM"
+$FNXMGR --flash $ROM --address 0x000000
--- upload-to-mem.sh
+++ upload-to-mem.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-FNXMGR="python3 $FOENIXMGR/FoenixMgr/fnxmgr.py --port /dev/ttyUSB1"
-$FNXMGR --upload-srec ../EmuTOS-RAM-loader/ldemutos.s28 ; $FNXMGR --binary emutos-a2560m.rom --address 0x100000
+COM=/dev/ttyUSB1
+FNXMGR="python3 $FOENIXMGR/FoenixMgr/fnxmgr.py --port $COM"
+ROM=`ls emutos-a2560?.rom`
+echo "Uploading $ROM to memory through port $COM"
+#ldemutos.s28 is a simple program that sets the reset vector to 0x100000. There
+#are probably other/better ways of doing that.
+$FNXMGR --upload-srec tools/ldemutos.s28 ; $FNXMGR --binary $ROM --address 0x100000
