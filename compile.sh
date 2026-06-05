1579a578bcfd942e7e89c3a1c3aa658998a0cf67
Vincent Barrilliot <vincent.barrilliot@laposte.net>
2026-06-05 21:26:57 +0200

    Adapt compile.sh so it builds by default for the K since it's what has traction at the moment

--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ pushd foenix
 rm libfoenix.a
 make -j4
 popd
-rm -f emutos.img emutos-a2560m.rom
+rm -f emutos.img emutos-a2560k.rom
 # Change "fr" to your country code and 4 to the number of CPU cores you have
-make UNIQUE=fr a2560m -j4
+make UNIQUE=fr a2560k -j4
